2a7d43247e08e54687c8c0099a5d06cb8a39301e
Luciann N <142541699+ln5711@users.noreply.github.com>
2023-08-31 15:50:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Function to display the selected date
+function displayDate() {
+    const dateInput = document.getElementById('datePicker');
+    const dateValue = dateInput.value;
+    const output = document.getElementById('selectedDate');
+    if (dateValue) {
+        output.textContent = `You selected: ${dateValue}`;
+    } else {
+        output.textContent = 'No date selected';
+    }
+}
+
+// Function to handle tab toggling
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Function to store deposit
+function storeDeposit() {
+    const depositAmount = document.getElementById('depositAmount').value;
+    const dateValue = document.getElementById('datePicker').value;
+    if (depositAmount && dateValue) {
+        const depositTableBody = document.querySelector('#depositTable tbody');
+        const newRow = depositTableBody.insertRow();
+        const dateCell = newRow.insertCell(0);
+        const amountCell = newRow.insertCell(1);
+        dateCell.textContent = dateValue;
+        amountCell.textContent = depositAmount;
+    } else {
+        alert('Please enter both date and deposit amount!');
+    }
+}
